45870ea77b69661da8292cb406715468a64aeaef
Welly W <winatawelly16@gmail.com>
2020-01-19 01:29:14 +0700

    fix

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="masthead">
             <div className="container d-flex h-100 align-items-center">
                 <div className="mx-auto text-center">
-                    <h1 className="mx-auto my-0 text-uppercase"><Typical steps={['Watch', 1000, 'WatchNow', 500]} loop={2} wrapper="p"/></h1>
+                    <h1 className="mx-auto my-0 text-uppercase">WatchNow</h1>
                     <h2 className="text-white-50 mx-auto mt-2 mb-5">Your next favourite movies,
                         <br/>just a few steps away.</h2>
                     <Link to="/recommend" className="btn btn-primary js-scroll-trigger">Get Started</Link>
